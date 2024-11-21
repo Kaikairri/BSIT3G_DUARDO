651bfd5130a5ddf5e40ce85ef616bd3caf9f2ac9
Kaikairri <duardo.kairric@gmail.com>
2024-11-21 21:11:42 +0800

    update

---
+++ 3. javascript folder/script.js
@@ -0,0 +1,7 @@
+let hamMenu = document.querySelector('.ham-menu');
+let sideBar = document.querySelector('.sidebar');
+
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');
+    sideBar.classList.toggle('active');
+});
