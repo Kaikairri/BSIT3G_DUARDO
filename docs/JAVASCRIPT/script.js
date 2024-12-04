ee9b22b4373eefa65a3f24ceec429cc7792853d9
Kaikairri <127919025+Kaikairri@users.noreply.github.com>
2024-12-04 18:04:52 +0800

    Delete docs directory

--- docs/JAVASCRIPT/script.js
+++
@@ -1,7 +0,0 @@
-let hamMenu = document.querySelector('.ham-menu');
-let sideBar = document.querySelector('.sidebar');
-
-hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active');
-    sideBar.classList.toggle('active');
-});
